6d86e7da7e375a5882c4a5b84aed81d86197287e
Ryan <120701017+RyanHarang@users.noreply.github.com>
2023-09-01 10:56:22 -0700

    Filtering Elsewhere

--- Weather/public/filter.py
+++
@@ -1,7 +0,0 @@
-with open("cities15000.txt", "r") as file:
-    lines = file.readlines()
-
-filtered_lines = [line for line in lines if int(line.split("\t")[14]) >= 50000]
-
-with open("filtered_cities.txt", "w") as file:
-    file.writelines(filtered_lines)
