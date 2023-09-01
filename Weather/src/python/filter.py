3d627f4ccb8b4df30848daeae852b80854adfe0c
Ryan <120701017+RyanHarang@users.noreply.github.com>
2023-09-01 11:49:29 -0700

    ReadMe and comments

--- Weather/src/python/filter.py
+++ Weather/src/python/filter.py
@@ -1,6 +1,7 @@
+# Must bring file in to be filtered and set name accordingly
 with open("cities15000.txt", "r", encoding="utf-8") as file:
     lines = file.readlines()
-
+# Choose a population number to be the minimum for a city to be displayed
 filtered_lines = [line for line in lines if int(line.split("\t")[14]) >= 85000]
 
 with open("filtered_cities.txt", "w", encoding="utf-8") as file:
